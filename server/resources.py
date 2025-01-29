2c3c466086b3d6abe2f8419558587898cc2777ba
Marion <wekesamarion100@gmail.com>
2025-01-29 16:33:48 +0300

    added helper function for validation

--- server/resources.py
+++ server/resources.py
@@ -1,3 +1,11 @@
 from flask_restful import Resource, reqparse
 from models import db, Driver, Circuit, Stat, DriverCircuit
 from flask import jsonify
+
+# Helper function for data validation
+def validate_number(value):
+    try:
+        float(value)
+        return value
+    except ValueError:
+        raise ValueError("Value must be a number")
