508212941459b2818ca3b6adb93ab21203a6405f
Marion <wekesamarion100@gmail.com>
2025-01-29 16:43:25 +0300

    populated the database with initial data

--- server/seed.py
+++ server/seed.py
@@ -1,2 +1,13 @@
 from app import db, app
 from models import Driver, Circuit, Stat
+
+with app.app_context():
+    db.create_all()
+
+    drivers = [Driver(name="Lewis Hamilton", age=36, team="Mercedes"), Driver(name="Max Verstappen", age=25, team="Red Bull")]
+    circuits = [Circuit(name="Monaco GP", location="Monaco", length=3.34), Circuit(name="Silverstone", location="UK", length=5.89)]
+
+    db.session.add_all(drivers + circuits)
+    db.session.commit()
+
+    print("Database seeded!")
