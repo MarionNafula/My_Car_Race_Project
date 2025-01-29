5683ef02666b816640c6e7d3b404390ac32c1651
Marion <wekesamarion100@gmail.com>
2025-01-29 16:20:55 +0300

    added circuit table

--- server/models.py
+++ server/models.py
@@ -12,3 +12,13 @@ class Driver(db.Model, SerializerMixin):
     stats = db.relationship("Stat", backref="driver", lazy=True)
 
     serialize_rules = ('-stats.driver',)
+
+class Circuit(db.Model, SerializerMixin):
+    __tablename__ = 'circuits'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    length = db.Column(db.Float, nullable=False)
+    stats = db.relationship("Stat", backref="circuit", lazy=True)
+
+    serialize_rules = ('-stats.circuit',)
