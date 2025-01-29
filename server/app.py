3af8abba4ded33b9b6121dae26447d9448e66413
Marion <wekesamarion100@gmail.com>
2025-01-29 16:01:45 +0300

    initialized flask

--- server/app.py
+++ server/app.py
@@ -3,3 +3,6 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
