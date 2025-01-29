9eaf54d69d700c3ae5796cb8df3ca60d3983dc8d
Marion <wekesamarion100@gmail.com>
2025-01-29 16:17:00 +0300

    added driver table

--- server/models.py
+++ server/models.py
@@ -2,3 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
+
+class Driver(db.Model, SerializerMixin):
+    __tablename__ = 'drivers'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    team = db.Column(db.String(100), nullable=False)
+    stats = db.relationship("Stat", backref="driver", lazy=True)
+
+    serialize_rules = ('-stats.driver',)
