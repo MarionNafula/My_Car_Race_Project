dcc0a29cf19865d590faedd793a172235f82a417
Marion <wekesamarion100@gmail.com>
2025-01-29 16:23:02 +0300

    added drivercircuit table

--- server/models.py
+++ server/models.py
@@ -22,3 +22,12 @@ class Circuit(db.Model, SerializerMixin):
     stats = db.relationship("Stat", backref="circuit", lazy=True)
 
     serialize_rules = ('-stats.circuit',)
+
+class DriverCircuit(db.Model, SerializerMixin):
+    __tablename__ = 'driver_circuit'
+    id = db.Column(db.Integer, primary_key=True)
+    driver_id = db.Column(db.Integer, db.ForeignKey("drivers.id"), nullable=False)
+    circuit_id = db.Column(db.Integer, db.ForeignKey("circuits.id"), nullable=False)
+    race_date = db.Column(db.Date, nullable=False)
+
+    serialize_rules = ('-driver.stats', '-circuit.stats')
